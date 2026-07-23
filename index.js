7804427113f8bf7dea1383d41479e778ee7aae68
mayconalmeida2020-prog <mayconalmeida2020@gmail.com>
2026-07-23 17:00:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,137 @@
-console.log("🤖 Agente de análise iniciado");
+const axios = require("axios");
 
-const agora = new Date();
+
+const API_KEY = process.env.TWELVEDATA_KEY;
+
+
+const PAR = "EUR/USD";
+
+
+async function buscarCandles() {
+
+  const url =
+  `https://api.twelvedata.com/time_series?symbol=${encodeURIComponent(PAR)}&interval=1min&apikey=${API_KEY}&outputsize=100&timezone=America/Cuiaba`;
+
+
+  const resposta = await axios.get(url);
+
+
+  if (!resposta.data.values) {
+
+    console.log("Erro ao buscar candles");
+    console.log(resposta.data);
+
+    return [];
+
+  }
+
+
+  return resposta.data.values.reverse();
+
+}
+
+
+
+function analisar(candles){
+
+
+  if(candles.length < 3){
+
+    return null;
+
+  }
+
+
+  const atual = candles[candles.length-1];
+
+  const anterior = candles[candles.length-2];
+
+
+  const compra =
+    parseFloat(atual.close) >
+    parseFloat(atual.open)
+    &&
+    parseFloat(anterior.close) <
+    parseFloat(anterior.open);
+
+
+
+  const venda =
+    parseFloat(atual.close) <
+    parseFloat(atual.open)
+    &&
+    parseFloat(anterior.close) >
+    parseFloat(anterior.open);
+
+
+
+  if(compra){
+
+    return {
+      sinal:"COMPRA",
+      preco:atual.close
+    };
+
+  }
+
+
+
+  if(venda){
+
+    return {
+      sinal:"VENDA",
+      preco:atual.close
+    };
+
+  }
+
+
+
+  return null;
+
+
+}
+
+
+
+
+async function iniciar(){
+
+
+console.log("🤖 Agente iniciado");
+
+
+const candles =
+await buscarCandles();
+
+
+
+const resultado =
+analisar(candles);
+
+
+
+if(resultado){
+
+
+console.log("🚨 NOVO SINAL");
+
+console.log(resultado);
+
+
+}else{
+
 
 console.log(
-  "Execução:",
-  agora.toLocaleString("pt-BR")
+"Sem sinal neste momento"
 );
 
-console.log(
-  "Sistema funcionando corretamente."
-);
+
+}
+
+
+
+}
+
+
+iniciar();
